c97f170e2d0b45890cd8b02c83e722dff396dc56
dukerspace <montolsaklor@gmail.com>
2018-06-05 14:53:12 +0700

    Update permission.sh

--- permission.sh
+++ permission.sh
@@ -1,1 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -e
+sudo chmod -R 777 app/app/etc/
+sudo chmod -R 777 app/var/
+sudo chmod -R 777 app/pub/media/
+sudo chmod -R 777 app/pub/static/
+sudo chmod -R 777 app/generated/
+
+DONE="Permission rewrite"
+echo $DONE
--- readme.md
+++ readme.md
@@ -2,5 +2,7 @@
 - copy .env-dist > .env
 - set up environtment 
 - docker-compose up -d
+- extract magento2 to folder app
+- run bash ./permission.sh
 - set up magento 2
 - enjoy the code.
